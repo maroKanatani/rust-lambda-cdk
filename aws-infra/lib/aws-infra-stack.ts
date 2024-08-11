f5d9e3868ab3040f3e3679a4aaf036ba8f930b63
maroKanatani <16130443+maroKanatani@users.noreply.github.com>
2024-08-11 22:51:02 +0900

    convert to container based lambda

--- aws-infra/lib/aws-infra-stack.ts
+++ aws-infra/lib/aws-infra-stack.ts
@@ -2,27 +2,35 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import path = require("path");
 
 export class AwsInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Define the Lambda function resource
-    const helloWorldFunction = new lambda.Function(this, "HelloWorldFunction", {
-      runtime: lambda.Runtime.NODEJS_20_X,
-      code: lambda.Code.fromAsset("lambda"),
-      handler: "hello.handler",
-    });
-
+    const helloWorldFunction = new lambda.DockerImageFunction(
+      this,
+      "HelloWorldFunction",
+      {
+        code: lambda.DockerImageCode.fromImageAsset(
+          path.join(__dirname, "../lambda"),
+          {
+            cmd: ["hello.handler"],
+          }
+        ),
+        // For ARM-based Lambda functions, specify the architecture
+        architecture: lambda.Architecture.ARM_64,
+      }
+    );
 
     // Define the API Gateway resource
-    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+    const api = new apigateway.LambdaRestApi(this, "HelloWorldApi", {
       handler: helloWorldFunction,
       proxy: false,
     });
-        
+
     // Define the '/hello' resource with a GET method
-    const helloResource = api.root.addResource('hello');
-    helloResource.addMethod('GET');
+    const helloResource = api.root.addResource("hello");
+    helloResource.addMethod("GET");
   }
 }
